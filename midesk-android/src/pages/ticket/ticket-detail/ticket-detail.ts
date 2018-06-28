a32a1083a7c31c24198f0a5856c23d4e2a5e9c6b
QuangSon <quangson2210@gmail.com>
2018-06-28 16:51:18 +0700

    push

--- midesk-android/src/pages/ticket/ticket-detail/ticket-detail.ts
+++ midesk-android/src/pages/ticket/ticket-detail/ticket-detail.ts
@@ -108,6 +108,7 @@ export class TicketDetailPage {
   listenEventUpdateTicket(){
       this._socketService.listenEvent('NEW_UPDATE_TICKET').subscribe(data=>{
       console.log(data);
+      
       let arr:any = data;
       for(let i=0;i<arr.length;i++){
         //console.log(this.navParamsCtrl.get('data'));
@@ -202,7 +203,7 @@ export class TicketDetailPage {
       }
       //console.log(data[0]['ticket_id']+'------'+this.navParamsCtrl.get('data').id);
       //if(this.navCtrl.getActive().name == 'TicketDetailPage' && data[0]['ticket_id'] == this.navParamsCtrl.get('data').id && JSON.parse(data[0].content)['createby']['id'] != this._authService.getLoggedInUser().id){
-      if(data[0]['ticket_id'] == this.navParamsCtrl.get('data').id && this.navCtrl.getActive().name == 'TicketDetailPage'){
+      if(this.navCtrl.getActive().name=='TicketDetailPage' && data[0]['ticket_id'] == this.navParamsCtrl.get('data').id && JSON.parse(data[0].content)['createby']['id'] != this._authService.getLoggedInUser().id){
         this.ticketUpdate = this.ticketUpdateDetail = [];
         this.countChange = Object.keys(this.ticketUpdateDetail).length + Object.keys(this.ticketUpdate).length;
         this._dataService.createToast('Thông tin phiếu vừa được thay đổi bởi ' + JSON.parse(data[0].content)['createby']['name']+'.',3000,'fail-toast');
