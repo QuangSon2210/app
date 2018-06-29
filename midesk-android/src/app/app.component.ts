27929314de68a0fb7c241cde8304c0618b7f0631
QuangSon <quangson2210@gmail.com>
2018-06-29 17:18:09 +0700

    push

--- midesk-android/src/app/app.component.ts
+++ midesk-android/src/app/app.component.ts
@@ -186,7 +186,8 @@ export class MyApp {
   }
   handleNotification(){
     this._localNotification.on('click').subscribe(res=>{
-      this.nav.push(TicketDetailPage,{data:{data:res.data.id}});
+      let index = { id: res.data.id };
+      this.nav.push(TicketDetailPage,{data:index,component:'TicketDetailPage'});
     })
   }
   listenEventUpdate(){
