4cdd0f4c1716df08b717fe3d42d0c088dfe667c7
QuangSon <quangson2210@gmail.com>
2018-06-29 15:12:42 +0700

    push

--- midesk-android/src/app/app.component.ts
+++ midesk-android/src/app/app.component.ts
@@ -67,7 +67,7 @@ export class MyApp {
         this.connectSocket();
         this.listenEventNewNotifi();
         this.listenEventUpdate();
-        //this.receiveNotification();
+        this.receiveNotification();
         this._notifyService.countNewNotifications().subscribe(res=>{ this.countNotify = res;});
         this.loggedInUser = this._authService.getLoggedInUser();
         this.avatarName = this._authService.getLoggedInUser().lastname;
